6d78253e6a278788a8baef77f95e3b6ef636cb07
judy <judy@Users-MacBook-Pro-4.local>
2019-04-21 20:46:09 -0700

    Added about and services section

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const appConfig = require("./config/main-config.js");
 const routeConfig = require("./config/route-config.js");
 
 app.use(express.static('/src/public'));
-path.join(__dirname, 'public');
+
 
 appConfig.init(app, express);
 routeConfig.init(app);
